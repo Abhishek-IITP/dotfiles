f8439b18587bb976be2c066d74f761a680296e65
Abhishek-IITP <abhishekmohanty7325@gmail.com>
2026-02-06 14:27:24 +0530

    some small changes in nvim config

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -8,3 +8,10 @@ local map = vim.keymap.set
 
 map("v", "K", ":m '<-2<CR>gv=gv") --use can use alt+ J also to move-up
 map("v", "J", ":m '>+1<CR>gv=gv") --use can use alt+ J also to move-down
+
+local keymap = vim.keymap -- for conciseness
+--keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+keymap.set("n", "<leader>tw", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
